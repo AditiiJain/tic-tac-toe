12e15cecf808680f734fb9ea5be8a3ca4e29edff
AditiiJain <iaditi.jain08@gmail.com>
2021-08-29 13:33:27 +0530

    game setup

---
+++ src/components/Icon.component.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {FaTimes,FaPen,FaRegCircle} from "react-icons/fa";
+
+const Icon = () => {
+    return ( <>
+   
+    <FaTimes/>
+    </> );
+}
+ 
+export default Icon;
